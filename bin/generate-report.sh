df4ac002a8f3b633a6618525056274b6ca03bb03
Heowbert <heow@heow.net>
2025-01-29 12:53:44 -0500

    add simple report

---
+++ bin/generate-report.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for file in "/home/pi/budget-network-monitor/log"/????-??-??*.log; do
+    if [[ -f "$file" ]]; then
+        line_count=$(grep -v " 0 packets" "$file"| wc -l )
+        echo "$file: $line_count lines"
+    fi
+done
