0d0558dc08a262b181903180e19e6a262c4ac8a4
Heowbert <heow@heow.net>
2025-01-28 21:10:25 -0500

    added stop

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+
+LOG="log/monitor.log"
+MAC="04:42:1A:09:4A:A5"
+
+if [ -d "log" ]; then
+    echo "logging to $LOG"
+else
+    echo "log directory not found"
+    exit 1
+fi
+
+# warm up sudo
+sudo ls ./log
+
+# take a 10 second sample every minute
 while true; do
-    echo -n "$(date '+%Y-%m-%d %H:%M:%S') " | tee -a monitor.log
-    timeout 10 tcpdump -i wlp1s0 ether host 04:42:1A:09:4A:A5 2>&1 | grep captured | tee -a monitor.log
+    echo -n "$(date '+%Y-%m-%d %H:%M:%S') " | tee -a $LOG
+    timeout 10 sudo tcpdump -i wlp1s0 ether host $MAC 2>&1 | grep captured | tee -a $LOG
     sleep 50
 done
 
 exit
 
+# TODO
 # split it into days
 awk -F '\ ' '{print > "log-" $1 ".txt"}' sample1.log
 
