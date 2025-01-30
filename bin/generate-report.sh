ac342638d930e4f9f61699ca475a761664f2b7bc
Heowbert <heow@heow.net>
2025-01-30 11:32:37 -0500

    clean up output

--- bin/generate-report.sh
+++ bin/generate-report.sh
@@ -2,6 +2,6 @@
 for file in "/home/pi/budget-network-monitor/log"/????-??-??*.log; do
     if [[ -f "$file" ]]; then
         line_count=$(grep -v " 0 packets" "$file"| wc -l )
-        echo "$file: $line_count minutes"
+        echo "${file:36}: $line_count minutes"
     fi
 done
